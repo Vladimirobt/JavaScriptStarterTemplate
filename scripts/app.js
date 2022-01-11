d97854dbb91cf59ca9186c478174e855e6ee03ce
VladimirOBT <Vladimirobt@gmail.com>
2022-01-11 15:58:09 -0700

    Added basic variables

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+console.log("Yee Haw Lads");
+
+let suit = "spades";
+let face = "queen";
+let value = 10;
+
+let isface = true;
+
+let aceOfSpades = {
+	suit: "spades",
+	face: "ace",
+	value: 1,
+	isface: false,
+};
+
+console.log(aceOfSpades.toString());
